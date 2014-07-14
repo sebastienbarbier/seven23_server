bec102d45622bdf9e88d9276d5b493c465a3614a
Sébastien BARBIER <contact@sebastienbarbier.com>
2014-07-15 00:06:42 +0200

    remove .settings, and add 723e_web

---
+++ 723e_web/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 723eWebApp
+ * @description
+ * # 723eWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('723eWebApp', []);
---
+++ 723e_web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 723eWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 723eWebApp
+ */
+angular.module('723eWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
