9dbba70f918607236d993daddcec1b9892e408d0
Sébastien BARBIER <contact@sebastienbarbier.com>
2014-12-31 00:18:55 +0100

    Remove Console.log() in storage

--- 723e_web/app/scripts/storage.js
+++ 723e_web/app/scripts/storage.js
@@ -26,9 +26,6 @@ define("storage",
 		var fetchCounter = 0;
 
 		// Get User model
-		console.log(userid);
-		console.log(user);
-
 		user.set('id', userid);
 		user.fetch({
 	        success: function (u) {
