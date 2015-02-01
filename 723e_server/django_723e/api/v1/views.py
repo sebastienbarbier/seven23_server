2381a8960001de8dc82da1f04394e8c7afad8b20
Sébastien BARBIER <contact@sebastienbarbier.com>
2015-02-01 20:12:02 +0100

    Fix bug in year_resume monthly sum

--- 723e_server/django_723e/api/v1/views.py
+++ 723e_server/django_723e/api/v1/views.py
@@ -19,15 +19,15 @@ def resume_year(request):
         if year == None:
             year = datetime.datetime.today.year()
 
-        list = DebitsCredits.objects.filter(date__year=year, reference_amount__isnull=False).extra(select={'month': "EXTRACT(month from date)"}).values('month').annotate(count=Count('amount'), sum=Sum('reference_amount'))
-        print(list)
+        list_months = DebitsCredits.objects.filter(date__year=year, reference_amount__isnull=False).extra(select={'month': "EXTRACT(month from date)"}).values('month').annotate(count=Count('reference_amount'), sum=Sum('reference_amount'))
+
         months = {}
-        for i in list:
+        for i in list_months:
             month_number = int(i['month'])
             months[month_number] = {}
             months[month_number]['count'] = i['count']
             months[month_number]['sum'] = i['sum']
-            list2 = DebitsCredits.objects.filter(date__year=year, date__month=month_number, amount__lt=0, reference_amount__isnull=False).extra(select={'month': "EXTRACT(month from date)"}).values('category').annotate(count=Count('amount'), sum=Sum('reference_amount'))
+            list2 = DebitsCredits.objects.filter(date__year=year, date__month=month_number, amount__lt=0, reference_amount__isnull=False).extra(select={'month': "EXTRACT(month from date)"}).values('month').annotate(count=Count('reference_amount'), sum=Sum('reference_amount'))
 
             if len(list2) > 0:
                 months[month_number]['sum_debits'] = list2[0]['sum']
