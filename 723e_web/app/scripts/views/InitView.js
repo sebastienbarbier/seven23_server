35a391f7650425d30f187b9380eb3c847fcadc85
Sébastien BARBIER <contact@sebastienbarbier.com>
2015-04-16 22:27:52 +0200

    Implement responsive navbar menu

--- 723e_web/app/scripts/views/InitView.js
+++ 723e_web/app/scripts/views/InitView.js
@@ -12,6 +12,18 @@ define([
 		render: function() {
 			var template = Mustache.render(initTemplate, {});
 			this.$el.html(template);
+
+			// BIND EVENT ON MENU
+			$('.menu-button-smartphone a').on('click', function() {
+				$("#navbar nav").toggleClass('open');
+				return false;
+			});
+			$("#navbar li a").on('click', function () {
+				if($("#navbar nav").hasClass('open')) {
+					$("#navbar nav").removeClass('open')
+				}
+			});
+
 		},
 
 		isLoaded: function() {
