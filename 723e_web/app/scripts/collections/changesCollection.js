4cebae173604220ba79e47d28f93444e4b3b0fe4
Sébastien BARBIER <contact@sebastienbarbier.com>
2015-09-02 18:58:01 +0200

    Remove 723e_web from server repo

--- 723e_web/app/scripts/collections/changesCollection.js
+++
@@ -1,9 +0,0 @@
-define(["jquery", "backbone", "ws", "changesModel"], function($, Backbone, WebServices, ChangesModel) {
-
-	var changes = Backbone.Collection.extend({
-		model: ChangesModel,
-		url: WebServices.v1.changes
-	});
-
-	return changes;
-});
--- 723e_web/app/scripts/collections/currenciesCollection.js
+++
@@ -1,9 +0,0 @@
-define(["jquery", "backbone", "ws", "currencyModel"], function($, Backbone, WebServices, CurrencyModel) {
-
-	var currencies = Backbone.Collection.extend({
-		model: CurrencyModel,
-		url: WebServices.v1.currencies
-	});
-
-	return currencies;
-});
