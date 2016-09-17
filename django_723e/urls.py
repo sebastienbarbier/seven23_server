210aeec15c8ef16572e19fb12485b1c6ae7aad8a
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-09-17 19:21:41 +0700

    Add static delivery for admin css

--- django_723e/urls.py
+++ django_723e/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 
 from django.contrib import admin
 
@@ -18,4 +20,4 @@ urlpatterns = [
 
     url(r'^api/', include("django_723e.api.urls")),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
