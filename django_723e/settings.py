d303c63b2d868dbe66b8d34f99a2c8a436fbc2a2
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-09-17 18:52:45 +0700

    Remove static file from settings.py. Was not used.

--- django_723e/settings.py
+++ django_723e/settings.py
@@ -65,14 +65,6 @@ TEMPLATES = [
     },
 ]
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
-)
-
 # Fix problem when creating new database
 MIGRATION_MODULES = {
   'auth': 'django_723e.migrations.auth',
