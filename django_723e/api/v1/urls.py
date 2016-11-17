b93794ba1e4cd173e52c9e3a8980fd0c1ca9b3e6
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-11-17 15:33:05 +0700

    Remove resume_year for dashboard
    Resume_year was calculating and doing some analytics to display on client side.
    Since we removed foreign_amount from translation, those calculation are
    no longer possible.

--- django_723e/api/v1/urls.py
+++ django_723e/api/v1/urls.py
@@ -4,7 +4,6 @@ from rest_framework import routers
 from django_723e.api.v1.accounts.views import api_accounts, api_users, subscription
 from django_723e.api.v1.currencies.views import api_currencies
 from django_723e.api.v1.transactions.views import api_categories, api_debitscredits, api_change
-from django_723e.api.v1.views import resume_year
 from django.contrib.auth import views as auth_views
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -16,9 +15,6 @@ router.register(r'debitscredits', api_debitscredits)
 router.register(r'changes', api_change)
 
 urlpatterns = [
-    # Examples:
-	url(r'resume_year/$', resume_year, name='resume_current_year'),
-	url(r'resume_year/(?P<year>[0-9]+)/$', resume_year, name='resume_specific_year'),
 	url(r'subscription/$', subscription, name='subscription'),
 
 	# URL used to send mail
