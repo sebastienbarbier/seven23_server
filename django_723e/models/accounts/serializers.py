48df6412a78586b3f4bddccb3da36b2519e0c715
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-11-17 15:32:10 +0700

    Make acount currency editable

--- django_723e/models/accounts/serializers.py
+++ django_723e/models/accounts/serializers.py
@@ -1,10 +1,11 @@
 from django_723e.models.accounts.models import Account, InvitationRequest
+from django_723e.models.currency.models import Currency
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 class AccountSerializer(serializers.HyperlinkedModelSerializer):
 
-    currency = serializers.PrimaryKeyRelatedField(read_only=True)
+    currency = serializers.PrimaryKeyRelatedField(queryset=Currency.objects.all())
 
     class Meta:
         model = Account
