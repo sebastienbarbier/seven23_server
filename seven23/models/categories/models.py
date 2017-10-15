768016ab3653799f41d0253aeb2bfd66fcf12359
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-10-15 10:17:05 +0700

    Remote MPTT from category models

--- seven23/models/categories/models.py
+++ seven23/models/categories/models.py
@@ -8,14 +8,14 @@ from django.utils.translation import ugettext as _
 
 from seven23.models.accounts.models import Account
 
-class Category(MPTTModel):
+class Category(models.Model):
     """
         Category of transaction.
     """
     account = models.ForeignKey(Account, related_name='categories', blank=True, null=True)
     name = models.CharField(_(u'Name'), max_length=128)
     description = models.TextField(_(u'Description'), blank=True, null=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
     selectable = models.BooleanField(_(u'Selectable'),
                                      default=True,
                                      help_text=_(u"Can be link to a transaction"))
@@ -29,13 +29,6 @@ class Category(MPTTModel):
     class MPTTMeta:
         order_insertion_by = ['name']
 
-    def move_children_right(self):
-        """ Move direct children categories to same level """
-        if self.get_children():
-            for i in self.get_children():
-                i.move_to(self, 'right')
-                i.save()
-
     def enable(self):
         """ Enable """
         self.active = True
@@ -43,7 +36,6 @@ class Category(MPTTModel):
 
     def disable(self):
         """ Disable """
-        self.move_children_right()
         self.active = False
         self.save()
 
@@ -59,7 +51,6 @@ class Category(MPTTModel):
             If a category object is link to a transaction, it cannot be delete because
             we need to keep trace of all transactions. It is only disable/hide.
         """
-        self.move_children_right()
         if self.transactions.all():
             self.toggle()
         else:
