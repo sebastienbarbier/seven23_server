0fbc4689fbd7ebc5269eaadda3048efbca124607
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-05-16 18:51:28 +0700

    Add AWS_S3_FILE_OVERWRITE as explicit

--- seven23/settings.py
+++ seven23/settings.py
@@ -66,6 +66,8 @@ else:
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_FILE_OVERWRITE = True
     # s3 static settings
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
