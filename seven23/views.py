237a0d8fd608125b148bab3a8a11189fa0dc8790
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-05-19 17:44:07 +0700

    Make price and trial period value dynamic based on settings file

--- seven23/views.py
+++ seven23/views.py
@@ -2,6 +2,7 @@
     Main views
 """
 import markdown2
+from seven23 import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -12,7 +13,7 @@ def home(request):
         Home page when trying to open server URL.
         Should confirm everything is ok, and provide a link to a client.
     """
-    return render(request, 'home.html')
+    return render(request, 'home.html', {"price": settings.PRICE_YEAR, "trial": settings.TRIAL_PERIOD})
 
 def legals(request):
     """
