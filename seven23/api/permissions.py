4ffb58466820cfb2569cf4d4837c8e48caed2c17
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-05-20 16:48:44 +0700

    Fix issue with imezone on IsPaid Permission

--- seven23/api/permissions.py
+++ seven23/api/permissions.py
@@ -1,7 +1,7 @@
 
 from itertools import chain
 from rest_framework import permissions
-from django.utils import timezone
+from datetime import datetime
 from seven23 import settings
 
 class CanWriteAccount(permissions.BasePermission):
@@ -34,4 +34,4 @@ class IsPaid(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return request.user.profile.valid_until > timezone.now()
+        return request.user.profile.valid_until > datetime.today()
