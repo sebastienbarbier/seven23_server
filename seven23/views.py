ce43bb9b91b17ee5c3fe8468340ef955ed786dba
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-06-23 16:02:25 +0800

    Add CSS gallery to see app screen without leaving the page

--- seven23/views.py
+++ seven23/views.py
@@ -13,7 +13,65 @@ def home(request):
         Home page when trying to open server URL.
         Should confirm everything is ok, and provide a link to a client.
     """
-    return render(request, 'home.html', {"price": settings.PRICE_YEAR, "trial": settings.TRIAL_PERIOD})
+
+    photos = [
+        {
+            "small": "images/screenshots/small/01-dashboard-desktop-light.png",
+            "large": "images/screenshots/large/01-dashboard-desktop-light.png",
+            "alt": "Seven23 dashboard desktop"
+        },
+        {
+            "small": "images/screenshots/small/01-dashboard-mobile-light.png",
+            "large": "images/screenshots/large/01-dashboard-mobile-light.png",
+            "alt": "Seven23 dashboard mobile"
+        },
+        {
+            "small": "images/screenshots/small/01-transactions-desktop-light.png",
+            "large": "images/screenshots/large/01-transactions-desktop-light.png",
+            "alt": "Seven23 transactions desktop"
+        },
+        {
+            "small": "images/screenshots/small/01-transactions-mobile-light.png",
+            "large": "images/screenshots/large/01-transactions-mobile-light.png",
+            "alt": "Seven23 transactions mobile"
+        },
+        {
+            "small": "images/screenshots/small/01-categories-desktop-light.png",
+            "large": "images/screenshots/large/01-categories-desktop-light.png",
+            "alt": "Seven23 categories desktop"
+        },
+        {
+            "small": "images/screenshots/small/01-categories-mobile-light.png",
+            "large": "images/screenshots/large/01-categories-mobile-light.png",
+            "alt": "Seven23 categories mobile"
+        },
+        {
+            "small": "images/screenshots/small/01-change-desktop-light.png",
+            "large": "images/screenshots/large/01-change-desktop-light.png",
+            "alt": "Seven23 change desktop"
+        },
+        {
+            "small": "images/screenshots/small/01-change-mobile-light.png",
+            "large": "images/screenshots/large/01-change-mobile-light.png",
+            "alt": "Seven23 change mobile"
+        },
+        {
+            "small": "images/screenshots/small/01-report-desktop-light.png",
+            "large": "images/screenshots/large/01-report-desktop-light.png",
+            "alt": "Seven23 report desktop"
+        },
+        {
+            "small": "images/screenshots/small/01-report-mobile-light.png",
+            "large": "images/screenshots/large/01-report-mobile-light.png",
+            "alt": "Seven23 report mobile"
+        }
+    ]
+
+    return render(request, 'home.html', {
+        "price": settings.PRICE_YEAR,
+        "trial": settings.TRIAL_PERIOD,
+        "photos": photos
+    })
 
 def legals(request):
     """
