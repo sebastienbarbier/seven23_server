e75934b98967fcd2084f4c5968773e9765b3c5b7
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-07-22 21:03:31 +0700

    Add a condition in settings.py if DATABSE_URL exsit before using dj_database_url

--- seven23/settings.py
+++ seven23/settings.py
@@ -93,7 +93,8 @@ LOGIN_URL = '/'
 # Database confugration using environment variable DATABASES_URL
 DATABASE_URL = os.environ.get('DATABASE_URL')
 DATABASES = {}
-DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
