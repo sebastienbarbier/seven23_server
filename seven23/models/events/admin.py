728488b8f27a49e544b456b945fe4e12046b35e0
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-08-16 17:55:48 +0700

    Remove dead code from goals and events

--- seven23/models/events/admin.py
+++
@@ -1,9 +0,0 @@
-"""
-    Profile administration
-"""
-from django.contrib import admin
-from seven23.models.events.models import Event, Attendee, EventToken
-
-admin.site.register(Event)
-admin.site.register(Attendee)
-admin.site.register(EventToken)
