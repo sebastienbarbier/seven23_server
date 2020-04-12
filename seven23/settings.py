f68efb0c1a7e2b8eae31be09b52805e3ae075bb9
Sébastien Barbier <contact@sebastienbarbier.com>
2020-04-12 14:33:58 +0200

    Update Django version to fix seurity issue

--- seven23/settings.py
+++ seven23/settings.py
@@ -118,6 +118,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages'
             ]
         }
     },
